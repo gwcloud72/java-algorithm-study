954d8a5387261fb310e69fac344bdac0c983b864
gwcloud72 <uu7271gw@gmail.com>
2026-02-25 02:31:49 +0900

    [programmers] PCCE 기출문제 2번 - 피타고라스 풀이

---
+++ programmers/00_basic/# PCCE 기출문제 2번 - 피타고라스.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        // b² = c² - a²
+        int b_square = c * c - a * a;
+
+        System.out.println(b_square);
+    }
+}
