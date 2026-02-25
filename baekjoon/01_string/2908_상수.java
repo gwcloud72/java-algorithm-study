77d12a6fcd7e228f5ff2a4b7e2662b3328b7bcac
gwcloud72 <uu7271gw@gmail.com>
2026-02-26 00:13:19 +0900

    [baekjoon] 3052 나머지 풀이 추가

---
+++ baekjoon/01_string/2908_상수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String a = st.nextToken();
+        String b = st.nextToken();
+
+        int ra = Integer.parseInt(new StringBuilder(a).reverse().toString());
+        int rb = Integer.parseInt(new StringBuilder(b).reverse().toString());
+
+        System.out.println(Math.max(ra, rb));
+    }
+}
