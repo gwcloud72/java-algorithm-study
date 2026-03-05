97cc5cfe5bd223c25d4527919fe8ca00784cef6c
gwcloud72 <uu7271gw@gmail.com>
2026-03-05 19:24:20 +0900

    [baekjoon] 2438_별찍기_1 풀이 추가

---
+++ baekjoon/00_basic/2438_별찍기1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+
+        for (int i = 1; i <= N; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
