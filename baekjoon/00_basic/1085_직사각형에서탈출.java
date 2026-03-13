fd41c4fe7435e4cbb5e27a3c8c151b17286ca2b3
gwcloud72 <uu7271gw@gmail.com>
2026-03-13 20:47:44 +0900

    [baekjoon] 1085_직사각형에서탈출 풀이 추가

---
+++ baekjoon/00_basic/1085_직사각형에서탈출.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min = Math.min(Math.min(x, y), Math.min(w - x, h - y));
+
+        System.out.println(min);
+    }
+}
